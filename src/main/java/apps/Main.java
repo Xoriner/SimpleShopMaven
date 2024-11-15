d46b533c47b3be60cbb2f81f6fc2cb2b9b19adcf
Michał Rodak <xoriner@gmail.com>
2024-11-15 07:46:27 +0100

    Moving to Maven

--- src/main/java/apps/Main.java
+++ src/main/java/apps/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         DAOImpl dao = new DAOImpl();
         dao.addOffer(new Offer("Balon", "10pm", "available"));
-        dao.deleteOffer(dao.getOffer(1));
+        //dao.deleteOffer(dao.getOffer(1));
     }
 }
